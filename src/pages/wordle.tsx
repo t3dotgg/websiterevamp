773ef48bffb11c76857628445a438db05854aa13
Blais3D <LinwoodBlaisdell@gmail.com>
2022-11-22 16:49:21 -0500

    some wordle work

--- src/pages/wordle.tsx
+++ src/pages/wordle.tsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
+import { Console, debug } from "console";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { root } from "postcss";
 import React, { useState, KeyboardEvent, useEffect } from "react";
 import { number } from "zod";
 import { trpc } from "../utils/trpc";
@@ -13,33 +15,164 @@ enum LetterStatus {
   Wrong = "bg-red-400",
 }
 
+const types: string[] = [
+  LetterStatus.Unknown,
+  LetterStatus.Unknown,
+  LetterStatus.Unknown,
+  LetterStatus.Unknown,
+  LetterStatus.Unknown,
+];
+
 type MyProps = {
-  letter: string;
-  status: LetterStatus;
+  currentWord: string;
+  correctWord: string;
+  pos: number;
+  currentPos: number;
 };
 
-function LetterBox(props: MyProps) {
-  const divClass: string =
-    "flex flex-col w-14 h-14 justify-center items-center p-6 rounded-md border-4 motion-safe:hover:scale-105 duration-500 " +
-    props.status;
-  return (
-    <div className={divClass}>
-      <h3 className="font-bold text-gray-600">{props.letter}</h3>
-    </div>
-  );
+type MyState = {
+  colors: string[];
+};
+class WordleBox extends React.Component<MyProps, MyState> {
+  allDone: boolean;
+  finalWord: string;
+
+  constructor(props: MyProps) {
+    super(props);
+
+    this.allDone = false;
+    this.finalWord = "";
+
+    this.state = {
+      colors: types,
+    };
+  }
+
+  render() {
+    if (this.props.pos == this.props.currentPos) {
+      this.finalWord = this.props.currentWord;
+    }
+
+    if (this.props.pos < this.props.currentPos && !this.allDone) {
+      this.setState({ colors: this.setColors() });
+      this.allDone = true;
+    }
+
+    let divClass0: string =
+      "flex flex-col w-14 h-14 justify-center items-center p-6 rounded-md border-4 motion-safe:hover:scale-105 duration-500 " +
+      this.state.colors[0];
+    let divClass1: string =
+      "flex flex-col w-14 h-14 justify-center items-center p-6 rounded-md border-4 motion-safe:hover:scale-105 duration-500 " +
+      this.state.colors[1];
+    let divClass2: string =
+      "flex flex-col w-14 h-14 justify-center items-center p-6 rounded-md border-4 motion-safe:hover:scale-105 duration-500 " +
+      this.state.colors[2];
+    let divClass3: string =
+      "flex flex-col w-14 h-14 justify-center items-center p-6 rounded-md border-4 motion-safe:hover:scale-105 duration-500 " +
+      this.state.colors[3];
+    let divClass4: string =
+      "flex flex-col w-14 h-14 justify-center items-center p-6 rounded-md border-4 motion-safe:hover:scale-105 duration-500 " +
+      this.state.colors[4];
+
+    return (
+      <div className="flex flex-row space-x-2">
+        <div className={divClass0}>
+          <h3 className="font-bold text-gray-600">{this.finalWord[0]}</h3>
+        </div>
+        <div className={divClass1}>
+          <h3 className="font-bold text-gray-600">{this.finalWord[1]}</h3>
+        </div>
+        <div className={divClass2}>
+          <h3 className="font-bold text-gray-600">{this.finalWord[2]}</h3>
+        </div>
+        <div className={divClass3}>
+          <h3 className="font-bold text-gray-600">{this.finalWord[3]}</h3>
+        </div>
+        <div className={divClass4}>
+          <h3 className="font-bold text-gray-600">{this.finalWord[4]}</h3>
+        </div>
+      </div>
+    );
+  }
+
+  returnSquare(i: number) {
+    if (i <= this.props.currentWord.length) {
+      return this.props.currentWord[i];
+    } else {
+      return " ";
+    }
+  }
+
+  countUp(counter: string) {
+    let ret = 0;
+    for (let i = 0; i < 5; i++) {
+      if (this.props.correctWord[i] == counter) {
+        ret++;
+      }
+    }
+    return ret;
+  }
+
+  setColors() {
+    let ret: string[] = [];
+    for (let i = 0; i < 5; i++) {
+      ret[i] = LetterStatus.Wrong;
+    }
+
+    //Go through and set all spaces correct to correct
+    for (let i = 0; i < 5; i++) {
+      if (this.finalWord[i] == this.props.correctWord[i]) {
+        ret[i] = LetterStatus.Correct;
+      }
+    }
+
+    //Go through and set all the missed
+    for (let i = 0; i < 5; i++) {
+      //only work on non-correct letters
+      if (ret[i] != LetterStatus.Correct) {
+        let howmany: number = this.countUp(this.finalWord[i] as string);
+        //If only one letter we must check that it has yet to be colored in
+        if (howmany != 0) {
+          let alreadyCon: number = 0;
+          for (let j = 0; j < 5; j++) {
+            if (
+              (ret[j] == LetterStatus.Correct ||
+                ret[j] == LetterStatus.Misplaced) &&
+              this.finalWord[j] == this.finalWord[i]
+            ) {
+              alreadyCon++;
+            }
+          }
+          if (alreadyCon < howmany) {
+            ret[i] = LetterStatus.Misplaced;
+          }
+        }
+      }
+    }
+    return ret;
+  }
 }
 
 const Home: NextPage = () => {
   const [theWord, setTheWord] = useState(
-    myWordsArray[
-      Math.round(Math.random() * myWordsArray.length + 0.5)
-    ]?.toUpperCase() as string
+    //myWordsArray[
+    //  Math.round(Math.random() * myWordsArray.length + 0.5)
+    //]?.toUpperCase() as string
+    "hello"
   );
 
   const [currentWord, setCurrentWord] = useState("");
+  const [guess, setGuess] = useState(0);
 
-  const handleKeyboardEvent = (e: KeyboardEvent<HTMLElement>) => {
-    setCurrentWord(e.key);
+  const handleKeyboardEvent = (e: React.KeyboardEvent<HTMLElement>) => {
+    if (e.key === "Backspace" && currentWord.length > 0) {
+      setCurrentWord(currentWord.substring(0, currentWord.length - 1));
+    } else if (e.key === "Enter" && currentWord.length == 5) {
+      setGuess(guess + 1);
+      setCurrentWord("");
+    } else if (e.key.length == 1 && currentWord.length < 5) {
+      setCurrentWord(currentWord + e.key);
+    }
   };
 
   return (
@@ -53,13 +186,45 @@ const Home: NextPage = () => {
       <main
         className="p-6 justify-center items-center overflow-hidden h-screen w-screen"
         tabIndex={0}
-        onKeyPressCapture={handleKeyboardEvent}
+        onKeyUpCapture={handleKeyboardEvent}
       >
-        <div className="container flex flex-wrap items-center justify-center gap-5">
-          <LetterBox
-            letter={currentWord}
-            status={LetterStatus.Unknown}
-          ></LetterBox>
+        <div className="container flex flex-col items-center justify-center gap-5">
+          <WordleBox
+            currentWord={currentWord}
+            correctWord={theWord}
+            pos={0}
+            currentPos={guess}
+          />
+          <WordleBox
+            currentWord={currentWord}
+            correctWord={theWord}
+            pos={1}
+            currentPos={guess}
+          />
+          <WordleBox
+            currentWord={currentWord}
+            correctWord={theWord}
+            pos={2}
+            currentPos={guess}
+          />
+          <WordleBox
+            currentWord={currentWord}
+            correctWord={theWord}
+            pos={3}
+            currentPos={guess}
+          />
+          <WordleBox
+            currentWord={currentWord}
+            correctWord={theWord}
+            pos={4}
+            currentPos={guess}
+          />
+          <WordleBox
+            currentWord={currentWord}
+            correctWord={theWord}
+            pos={5}
+            currentPos={guess}
+          />
         </div>
       </main>
     </>
